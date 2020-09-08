bde0081283fa29143c409e4ff7d0d09bbd9c79ec
easyopscyrilchen <cyrilchen@easyops.cn>
2020-09-08 09:38:04 +0800

    fix(script): optimize scripts

--- script/deploy/start_script.sh
+++ script/deploy/start_script.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+
+PACKAGE_NAME=h3c-switch-collector-plugin
+PACKAGE_PATH=$(dirname $(dirname "$(cd `dirname $0`; pwd)"))
+LOG_DIRECTORY=$PACKAGE_PATH/log
+LOG_FILE=$LOG_DIRECTORY/$PACKAGE_NAME.log
+
+
 if ! type getopt >/dev/null 2>&1 ; then
-  echo "Error: command \"getopt\" is not found" >&2
+  message="command \"getopt\" is not found"
+  echo "[ERROR] Message: $message" >& 2
+  echo "$(date "+%Y-%m-%d %H:%M:%S") [ERROR] Message: $message" > $LOG_FILE
   exit 1
 fi
 
-getopt_cmd=`getopt -o h -a -l exporter-host:,exporter-port: -n "start.sh" -- "$@"`
+getopt_cmd=`getopt -o h -a -l help:,exporter-host:,exporter-port: -n "start_script.sh" -- "$@"`
 if [ $? -ne 0 ] ; then
     exit 1
 fi
 eval set -- "$getopt_cmd"
+
 
 exporter_host="127.0.0.1"
 exporter_port=9116
@@ -59,21 +69,21 @@ do
             break
             ;;
         *)
-            echo "Error: argument \"$1\" is invalid" >&2
-            echo ""
+            message="argument \"$1\" is invalid"
+            echo "[ERROR] Message: $message" >& 2
+            echo "$(date "+%Y-%m-%d %H:%M:%S") [ERROR] Message: $message" > $LOG_FILE
             print_help
             exit 1
             ;;
     esac
 done
 
-if [ -f exporter.pid ]; then
-    echo "The SNMP exporter has already started."
-    exit 0
-fi
+mkdir -p $LOG_DIRECTORY
 
-chmod +x ./src/snmp_exporter
+message="start exporter"
+echo "[INFO] Message: $message"
+echo "$(date "+%Y-%m-%d %H:%M:%S") [INFO] Message: $message" >> $LOG_FILE
 
-./src/snmp_exporter --config.file="./src/snmp.yml" --web.listen-address="$exporter_host:$exporter_port" &
-
-echo $! > exporter.pid
+cd $PACKAGE_PATH/script
+chmod +x src/snmp_exporter
+./src/snmp_exporter --config.file=conf/snmp.yml --web.listen-address=$exporter_host:$exporter_port 2>&1 | tee -a $LOG_FILE
